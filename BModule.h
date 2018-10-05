4313bf9ba1887a79af0ad47b612520bc47a34908
yanyi0 <785144130@qq.com>
2018-10-05 17:05:51 +0800

    初始化子模块
    Signed-off-by: yanyi0 <785144130@qq.com>

---
+++ BModule.h
@@ -0,0 +1,13 @@
+//
+//  BModule.h
+//  GitSubmodules
+//
+//  Created by Cloud on 2018/10/5.
+//  Copyright © 2018年 Cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BModule : NSObject
+
+@end
---
+++ BModule.m
@@ -0,0 +1,13 @@
+//
+//  BModule.m
+//  GitSubmodules
+//
+//  Created by Cloud on 2018/10/5.
+//  Copyright © 2018年 Cloud. All rights reserved.
+//
+
+#import "BModule.h"
+
+@implementation BModule
+
+@end
